e72d95482cc2c8d035d6b35882d3c50752f6d77b
Yuan Zhi <zy8715@mun.ca>
2019-04-02 19:04:54 -0230

    get the balance data through the Api using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import AddressSearch from './AddressSearch';
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        console.log(term);
+    state = {balance: ''};
+
+    onSearchSubmit=  async(term) =>{
+        const response = await axios.get(`https://api.blockcypher.com/v1/btc/main/addrs/${term}/balance`);
+        this.setState({ balance: response.data.balance});
     }
 
     render(){
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
                 <AddressSearch onSubmit={this.onSearchSubmit}/>
+                <div>
+                    The balance is: {this.state.balance}
+                </div>
             </div>
         );
     }
